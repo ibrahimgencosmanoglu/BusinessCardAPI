b42b36909b7d1d0ddce8d2fea2af9112c4243a62
ibrahim gencosmanoglu <z.ibrahimgencosmanoglu@gmail.com>
2022-08-27 14:55:05 -0400

    Initial Commitment

---
+++ BusinessCardAPI/Models/BusinessCardContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessCardAPI.Models
+{
+    public class BusinessCardContext:DbContext
+    {
+        public BusinessCardContext(DbContextOptions<BusinessCardContext> options) 
+        :base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<BusinessCard> businessCards { get; set; }
+    }
+}
